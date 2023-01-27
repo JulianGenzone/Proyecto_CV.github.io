dc743c0025dba6c51832a6ed180a61573af5d1ca
JulianGenzone <123585578+JulianGenzone@users.noreply.github.com>
2023-01-27 19:23:03 -0300

    Add files via upload

---
+++ proyecto_final.js
@@ -0,0 +1,37 @@
+
+
+document.getElementById("comunicacion").addEventListener("click", function(){
+    document.getElementById("valor_comunicacion").style.display="block";
+});
+document.getElementById("comunicacionOcultar").addEventListener("click", function(){
+    document.getElementById("valor_comunicacion").style.display="none";
+});
+
+
+document.getElementById("solucion_de_conflictos").addEventListener("click", function(){
+    document.getElementById("valor_solucion_de_conflictos").style.display="block";
+});
+document.getElementById("solucion_de_conflictosOcultar").addEventListener("click", function(){
+    document.getElementById("valor_solucion_de_conflictos").style.display="none";
+});
+
+document.getElementById("desarrollo_de_farmacos").addEventListener("click", function(){
+    document.getElementById("valor_desarrollo_de_farmacos").style.display="block";
+});
+document.getElementById("desarrollo_de_farmacosOcultar").addEventListener("click", function(){
+    document.getElementById("valor_desarrollo_de_farmacos").style.display="none";
+});
+
+document.getElementById("organizacion").addEventListener("click", function(){
+    document.getElementById("valor_organizacion").style.display="block";
+});
+document.getElementById("organizacionOcultar").addEventListener("click", function(){
+    document.getElementById("valor_organizacion").style.display="none";
+});
+
+document.getElementById("liderazgo").addEventListener("click", function(){
+    document.getElementById("valor_liderazgo").style.display="block";
+});
+document.getElementById("liderazgoOcultar").addEventListener("click", function(){
+    document.getElementById("valor_liderazgo").style.display="none";
+});
